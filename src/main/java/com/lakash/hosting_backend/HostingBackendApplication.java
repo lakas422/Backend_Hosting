71240b5e496ff7b4299e2b953f9a6cd7826dd697
lakash shakya <lakashshakya.utecian@gmail.com>
2024-05-14 19:23:14 +0545

    Initial Commit

---
+++ src/main/java/com/lakash/hosting_backend/HostingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lakash.hosting_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lakash/hosting_backend/HostingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakash.hosting_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
